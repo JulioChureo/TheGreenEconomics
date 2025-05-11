c88deda445bd2fcc0eccd879833eed7664fd50b3
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2025-05-11 18:39:08 -0400

    ajustes query set de adminstracion

--- the_green_economics/apps/dashboards/views/articles_views.py
+++ the_green_economics/apps/dashboards/views/articles_views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
@@ -8,68 +10,69 @@ from django.views.generic import UpdateView
 from the_green_economics.apps.articles.forms import ArticleForm
 from the_green_economics.apps.articles.forms import DeleteArticleForm
 from the_green_economics.apps.articles.models import Article
+from the_green_economics.apps.utils.mixins import AdminUserMixin
+
+DASHBOARD_ARTICLES_QUERYSET = Article.objects.filter(
+    Q(status=Article.Status.PUBLISHED)
+    | Q(status=Article.Status.DRAFT)
+    | Q(status=Article.Status.UNDER_REVIEW),
+).order_by("-updated_at")
 
 
-class DashboardArticleListView(ListView):
+class DashboardArticleListView(AdminUserMixin, ListView):
     model = Article
     template_name = "dashboards/articles/article_list.html"
     context_object_name = "articles"
-
-    def get_queryset(self):
-        return Article.objects.filter().order_by(
-            "-publication_date",
-        )
+    queryset = DASHBOARD_ARTICLES_QUERYSET
+    paginator_class = Paginator
+    paginate_by = 10
 
 
 dashboard_article_list_view = DashboardArticleListView.as_view()
 
 
-class DashboardArticleDetailView(DetailView):
+class DashboardArticleDetailView(AdminUserMixin, DetailView):
     model = Article
     template_name = "dashboards/articles/article_retrieve.html"
     context_object_name = "article"
     slug_url_kwarg = "slug"
-
-    def get_queryset(self):
-        return Article.objects.all()
+    queryset = DASHBOARD_ARTICLES_QUERYSET
 
 
 dashboard_article_detail_view = DashboardArticleDetailView.as_view()
 
 
-class DashboardArticleCreateView(CreateView):
+class DashboardArticleCreateView(AdminUserMixin, CreateView):
     model = Article
     form_class = ArticleForm
     template_name = "dashboards/articles/article_create.html"
     success_url = reverse_lazy("articles:list")
+    queryset = DASHBOARD_ARTICLES_QUERYSET
 
 
 dashboard_article_create_view = DashboardArticleCreateView.as_view()
 
 
-class DashboardArticleUpdateView(UpdateView):
+class DashboardArticleUpdateView(AdminUserMixin, UpdateView):
     model = Article
     form_class = ArticleForm
     template_name = "dashboards/articles/article_update.html"
     success_url = reverse_lazy("articles:list")
     slug_url_kwarg = "slug"
-
-    def get_queryset(self):
-        return Article.objects.all()
+    context_object_name = "article"
+    queryset = DASHBOARD_ARTICLES_QUERYSET
 
 
 dashboard_article_update_view = DashboardArticleUpdateView.as_view()
 
 
-class DashboardArticleDeleteView(DeleteView):
+class DashboardArticleDeleteView(AdminUserMixin, DeleteView):
     model = Article
     form_class = DeleteArticleForm
     template_name = "dashboards/articles/article_delete.html"
     success_url = reverse_lazy("articles:list")
     slug_url_kwarg = "slug"
-
-    def get_queryset(self):
-        return Article.objects.all()
+    queryset = DASHBOARD_ARTICLES_QUERYSET
 
 
 dashboard_article_delete_view = DashboardArticleDeleteView.as_view()
--- the_green_economics/apps/dashboards/views/news_views.py
+++ the_green_economics/apps/dashboards/views/news_views.py
@@ -1,3 +1,6 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
@@ -8,68 +11,73 @@ from django.views.generic import UpdateView
 from the_green_economics.apps.news.forms import DeleteNewsForm
 from the_green_economics.apps.news.forms import NewsForm
 from the_green_economics.apps.news.models import News
+from the_green_economics.apps.utils.mixins import AdminUserMixin
+
+DASHBOARD_NEWS_QUERYSET = News.objects.filter(
+    Q(status=News.Status.PUBLISHED) | Q(status=News.Status.DRAFT),
+).order_by("-publication_date")
 
 
-class DashboardNewsListView(ListView):
+class DashboardNewsListView(AdminUserMixin, ListView):
     model = News
     template_name = "dashboards/news/news_list.html"
     context_object_name = "news"
-
-    def get_queryset(self):
-        return News.objects.filter().order_by(
-            "-publication_date",
-        )
+    queryset = DASHBOARD_NEWS_QUERYSET
+    paginate_by = 10
+    paginator_class = Paginator
 
 
 dashboard_news_list_view = DashboardNewsListView.as_view()
 
 
-class DashboardNewsDetailView(DetailView):
+class DashboardNewsDetailView(AdminUserMixin, DetailView):
     model = News
-    template_name = "dashboards/news/news_retrieve.html"
+    template_name = "dashboards/news/new_detail.html"
     context_object_name = "news"
     slug_url_kwarg = "slug"
-
-    def get_queryset(self):
-        return News.objects.all()
+    queryset = DASHBOARD_NEWS_QUERYSET
 
 
 dashboard_news_detail_view = DashboardNewsDetailView.as_view()
 
 
-class DashboardNewsCreateView(CreateView):
+class DashboardNewsCreateView(AdminUserMixin, CreateView):
     model = News
     form_class = NewsForm
     template_name = "dashboards/news/news_create.html"
     success_url = reverse_lazy("news:list")
+    queryset = DASHBOARD_NEWS_QUERYSET
 
 
 dashboard_news_create_view = DashboardNewsCreateView.as_view()
 
 
-class DashboardNewsUpdateView(UpdateView):
+class DashboardNewsUpdateView(AdminUserMixin, UpdateView):
     model = News
     form_class = NewsForm
     template_name = "dashboards/news/news_update.html"
     success_url = reverse_lazy("news:list")
     slug_url_kwarg = "slug"
-
-    def get_queryset(self):
-        return News.objects.all()
+    queryset = DASHBOARD_NEWS_QUERYSET
 
 
 dashboard_news_update_view = DashboardNewsUpdateView.as_view()
 
 
-class DashboardNewsDeleteView(DeleteView):
+class DashboardNewsDeleteView(AdminUserMixin, DeleteView):
     model = News
     form_class = DeleteNewsForm
     template_name = "dashboards/news/news_delete.html"
     success_url = reverse_lazy("news:list")
     slug_url_kwarg = "slug"
+    queryset = DASHBOARD_NEWS_QUERYSET
 
-    def get_queryset(self):
-        return News.objects.all()
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object = self.get_object()
+        self.object.status = News.Status.ARCHIVED
+        self.object.save()
+        return HttpResponseRedirect(success_url)
 
 
 dashboard_news_delete_view = DashboardNewsDeleteView.as_view()
