c98fcb23aa3ba35a5483dbd996f9bf1ce9ddd9f5
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2025-05-11 21:30:24 -0400

    eliminacion vistas no usadas

--- the_green_economics/apps/users/views.py
+++ the_green_economics/apps/users/views.py
@@ -1,53 +1,14 @@
-from typing import Any
-
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import QuerySet
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import CreateView
-from django.views.generic import DetailView
 from django.views.generic import FormView
-from django.views.generic import RedirectView
-from django.views.generic import UpdateView
 
-from the_green_economics.apps.users.forms import UserCreationForm
 from the_green_economics.apps.users.forms import UserLoginForm
 from the_green_economics.apps.users.forms import UserLogOutForm
-from the_green_economics.apps.users.models import User
-
-
-class UserCreateView(CreateView):
-    """Create user view.
-
-    Description: This view is used to create a new user.
-
-    Attributes:
-        model (User): The model used to create the user.
-        form_class (UserCreationForm): The form used to create the user.
-        template_name (str): The template used to render the view.
-        success_url (str): The URL to redirect to after the user is created.
-
-    Methods:
-        get_object (Any): Returns the object to be created.
-        get_success_url (str): Returns the URL to redirect to after the user is created.
-    """
-
-    model = User
-    form_class = UserCreationForm
-    template_name = "users/user_create.html"
-
-    def get_object(self, queryset: Any):
-        return super().get_object(queryset)
-
-    def get_success_url(self) -> str:
-        return reverse("users:login")
-
-
-user_create_view = UserCreateView.as_view()
 
 
 class UserLoginView(FormView):
@@ -146,86 +107,3 @@ class UserLogOutView(FormView, LoginRequiredMixin, SuccessMessageMixin):
 
 
 user_log_out_view = UserLogOutView.as_view()
-
-
-class UserDetailView(LoginRequiredMixin, DetailView):
-    """User detail view.
-
-    Description: This view is used to display the details of a user.
-
-    Attributes:
-        model (User): The model used to display the details of a user.
-        slug_field (str): The field used to generate the URL for the user.
-        slug_url_kwarg (str): The URL argument used to generate the URL for the user.
-        template_name (str): The template used to render the view.
-
-    Methods:
-        get_object (Any): Returns the user to be displayed.
-
-    """
-
-    model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
-    template_name = "users/user_detail.html"
-
-    def get_object(self):
-        return self.request.user
-
-
-user_detail_view = UserDetailView.as_view()
-
-
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    """User update view.
-
-    Description: This view is used to update the details of a user.
-
-    Attributes:
-        model (User): The model used to update the details of a user.
-        fields (list): The fields to be updated.
-        success_message (str): The message to display after the user is updated.
-
-    Methods:
-        get_success_url (str): Returns the URL to redirect to after the user is updated.
-        get_object (Any): Returns the user to be updated.
-
-    """
-
-    model = User
-    fields = ["name"]
-    success_message = _("Information successfully updated")
-    template_name = "users/user_update.html"
-
-    def get_success_url(self) -> str:
-        assert self.request.user.is_authenticated  # type guard
-        return self.request.user.get_absolute_url()  # type: ignore
-
-    def get_object(self, queryset: QuerySet | None = None) -> User:
-        assert self.request.user.is_authenticated  # type guard
-        return self.request.user
-
-
-user_update_view = UserUpdateView.as_view()
-
-
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-    """User redirect view.
-
-    Description: This view is used to redirect a user to their profile page.
-
-    Attributes:
-        permanent (bool): Whether the redirect should be permanent.
-
-    Methods:
-        get_redirect_url (str): Returns the URL to redirect to.
-
-    """
-
-    permanent = False
-
-    def get_redirect_url(self) -> str:
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
-
-
-user_redirect_view = UserRedirectView.as_view()
