f455b6ba35d656e1531828d3a8b76dd647b43cd5
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2025-05-11 21:42:50 -0400

    fix 4 sql consults, and implement cache

--- the_green_economics/apps/home/views.py
+++ the_green_economics/apps/home/views.py
@@ -1,6 +1,18 @@
+from django.core.cache import cache
 from django.views.generic import TemplateView
 
 from the_green_economics.apps.articles.models import Article
+
+HOME_QUERYSET = (
+    Article.objects.all()
+    .order_by("-created_at")[:15]
+    .values(
+        "id",
+        "title",
+        "slug",
+        "publication_date",
+    )
+)
 
 
 class HomeView(TemplateView):
@@ -8,9 +20,18 @@ class HomeView(TemplateView):
 
     template_name = "home/home.html"
 
+    def get_queryset(self):
+        queryset = cache.get_or_set(
+            "home-articles",
+            HOME_QUERYSET,
+            timeout=60 * 15,
+        )
+        if queryset is None:
+            queryset = HOME_QUERYSET
+            cache.set("home-articles", queryset, timeout=60 * 15)
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["new_articles"] = Article.objects.all().order_by("-created_at")[:4]
-        context["old_articles"] = Article.objects.all().order_by("-created_at")[5:11]
-        # context["news"] = News.objects.all().order_by("-created_at")[:5]
+        context["articles"] = list(self.get_queryset())
         return context
