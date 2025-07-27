87d678e40bf0596b82d2b29dabd5911223f32bad
Diego <diego.rivera.estefano@gmail.com>
2025-07-26 23:23:33 -0400

    add admin verify

--- the_green_economics/apps/dashboards/views/__init__.py
+++ the_green_economics/apps/dashboards/views/__init__.py
@@ -1,14 +1,14 @@
 from django.views.generic import TemplateView
 
+from the_green_economics.apps.utils.mixins import AdminUserMixin
 
-class DashboardHomeView(TemplateView):
+
+class DashboardHomeView(TemplateView, AdminUserMixin):
     template_name = "dashboards/home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context["articles"] = dashboard_article_list_view.get_queryset(self)
-        # context["news"] = dashboard_news_list_view.get_queryset(self)
-        return context
+    # def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    return context
 
 
 dashboard_home_view = DashboardHomeView.as_view()
