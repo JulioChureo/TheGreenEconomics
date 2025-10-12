ef7bdd64dc8b1abca2bc9a43cb7da6473b362504
Diego <diego.rivera.estefano@gmail.com>
2025-10-12 12:37:16 -0300

    add book read view and url

--- the_green_economics/apps/pages/urls.py
+++ the_green_economics/apps/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import about_view
+from .views import book_read_view
 from .views import book_view
 from .views import home_view
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path("", view=home_view, name="home"),
     path("about/", view=about_view, name="about"),
     path("book/", view=book_view, name="book"),
+    path("read/", view=book_read_view, name="book_read"),
 ]
